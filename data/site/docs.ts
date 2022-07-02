c99b030e7f1cdffbc75d648d8958da7e313d7a7e
pincman <pincman@qq.com>
2022-07-02 21:21:49 +0800

    修改blog sidebar的图片为直接加载

--- data/site/docs.ts
+++ data/site/docs.ts
@@ -25,7 +25,7 @@ export const courses: CourseCardItemType[] = [
     },
     {
         name: 'Rowfish使用教程',
-        image: require('@site/static/custom/rowfish-thumbnail.png'),
+        image: require('@site/static/custom/rowfish-thumbnail.png').default,
         href: '/docs/rowfish/overview',
         color: 'danger',
         status: 1,
